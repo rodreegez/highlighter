7baf157ec5631330e50e280c9cdd4cb779f2eb84
Adam <electronicbattleweapon7@gmail.com>
2021-12-28 12:21:05 +0000

    initial commit: scrape highlights to .json file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Highlighter
+
+Scrape your [Kindle highlights](https://read.amazon.com/notebook) and save them to a file.
+
+## Usage
+
+1. Clone this repo
+2. `cp .env{.example,}`
+3. Add your Amazon email address and password to `.env`
+4. `npm install` (requires [puppeteer](https://github.com/puppeteer/puppeteer)
+5. `node index.js`
+
+Your highlights will be saved to a file called `highlights.json` in your current working directory. The whole script can take a while to run.
---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://read.amazon.com/notebook')
+  await page.waitForSelector('#ap_email')
+  await page.type('#ap_email', process.env.EMAIL)
+  await page.type('#ap_password', process.env.PASSWORD)
+  await Promise.all([
+    page.click('#signInSubmit'),
+    page.waitForNavigation(),
+  ])
+
+  let books = []
+  let links = await page.$$('#kp-notebook-library > div > span > a')
+  var currentIndex = 1
+
+  while(currentIndex < links.length) {
+    console.log(`about to click link ${currentIndex}`)
+
+    await Promise.all([
+      page.click(`div#kp-notebook-library > div:nth-child(${currentIndex})`),
+      page.waitForTimeout(3000)
+    ])
+
+    console.log(`clicked link ${currentIndex}`)
+
+    let book = {}
+  	book['author'] = await page.$eval('div.a-span5 > p.a-spacing-none.kp-notebook-metadata', text => text.textContent);
+  	book['title'] = await page.$eval('h3', text => text.textContent);
+  	book['highlights'] = await page.$$eval('#highlight', highlights => {
+  		return highlights.map(highlight => highlight.textContent)
+  	})
+  	books.push(book)
+
+    console.log(`about to go back`)
+
+    await page.goto('https://read.amazon.com/notebook')
+
+    console.log(`back`)
+
+    currentIndex ++
+  }
+
+	console.log(books)
+
+  await page.screenshot({ path: 'screenshot.png' })
+
+  await browser.close()
+
+  fs.writeFileSync('highlights.json', JSON.stringify(books, null, 2))
+})()
